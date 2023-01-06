6fd1b39bd8153a0693c85be361f96b697982661d
Lucas Guimarães <guimaraeslucas242@gmail.com>
2023-01-06 14:32:23 -0300

    Number format helper test

--- lib/app/shared/utils/number_format_helper.dart
+++ lib/app/shared/utils/number_format_helper.dart
@@ -2,14 +2,18 @@ import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
 abstract class NumberFormatHelper {
-  static String formatCurrency(BuildContext context, [num? value]) {
-    return NumberFormat.currency(symbol: getCurrencySymbol(context))
+  static String formatCurrency(BuildContext context,
+      [num? value, Locale? locale]) {
+    String? stringLocale =
+        locale != null ? '${locale.languageCode}_${locale.countryCode}' : null;
+    return NumberFormat.currency(
+            locale: stringLocale, symbol: _getCurrencySymbol(context, locale))
         .format(value ?? 0);
   }
 
-  static String getCurrencySymbol(BuildContext context) {
+  static String _getCurrencySymbol(BuildContext context, Locale? locale) {
     return NumberFormat.simpleCurrency(
-            locale: Localizations.localeOf(context).toString())
+            locale: (locale ?? Localizations.localeOf(context)).toString())
         .currencySymbol;
   }
 }
---
+++ test/lib/app/shared/utils/number_format_helper_test.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:my_services/app/shared/utils/number_format_helper.dart';
+
+class MockBuildContext extends Mock implements BuildContext {}
+
+void main() {
+  late Locale locale;
+  late BuildContext context;
+
+  setUp(() {
+    context = MockBuildContext();
+  });
+
+  test('Should format int to pt_BR', () {
+    locale = const Locale('pt', 'BR');
+    const number = 22;
+    final result = NumberFormatHelper.formatCurrency(context, number, locale);
+
+    expect(result, equals('R\$\u{00A0}22,00'));
+  });
+
+  test('Should format double to pt_BR', () {
+    locale = const Locale('pt', 'BR');
+    const number = 17452.57;
+    final result = NumberFormatHelper.formatCurrency(context, number, locale);
+
+    expect(result, equals('R\$\u{00A0}17.452,57'));
+  });
+
+  test('Should format int to en_US', () {
+    locale = const Locale('en', 'US');
+    const number = 789;
+    final result = NumberFormatHelper.formatCurrency(context, number, locale);
+
+    expect(result, equalsIgnoringWhitespace('\$789.00'));
+  });
+
+  test('Should format double to en_US', () {
+    locale = const Locale('en', 'US');
+    const number = 7899945.357;
+    final result = NumberFormatHelper.formatCurrency(context, number, locale);
+
+    expect(result, equalsIgnoringWhitespace('\$7,899,945.36'));
+  });
+}
