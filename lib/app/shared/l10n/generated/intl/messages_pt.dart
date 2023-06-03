73f608d6c1f9b893e51b46fcc720b3040c774b85
Lucas Guimarães <guimaraeslucas242@gmail.com>
2023-06-03 19:33:41 -0300

    Fix: Add tip in the NoData Message

--- lib/app/shared/l10n/generated/intl/messages_pt.dart
+++ lib/app/shared/l10n/generated/intl/messages_pt.dart
@@ -134,9 +134,9 @@ class MessageLookup extends MessageLookupByLibrary {
         "noServiceTypes": MessageLookupByLibrary.simpleMessage(
             "Parece que você não registrou nenhum tipo de serviço,  clique no botão acima para registrar um novo."),
         "noServices": MessageLookupByLibrary.simpleMessage(
-            "Parece que você não registrou nenhum serviço, clique no botão acima para registrar um novo."),
+            "Parece que você não registrou nenhum serviço, clique no botão acima para registrar um novo.\n\nLembre-se, aqui você verá os serviços realizados hoje. Para visualizar outras datas, vá para a tela de serviços."),
         "noServicesHome": MessageLookupByLibrary.simpleMessage(
-            "Parece que você não registrou nenhum serviço para hoje, clique no botão acima para registrar um novo."),
+            "Parece que você não registrou nenhum serviço para hoje, clique no botão acima para registrar um novo.\n\nLembre-se, por padrão são exibidos os serviços que você realizou nesse mês. Tente alterar os filtros acima para visualizar datas diferentes."),
         "numberLesserThanZero": MessageLookupByLibrary.simpleMessage(
             "Por favor, informe um número maior ou igual a zero"),
         "onboardingSubtitle": MessageLookupByLibrary.simpleMessage(
