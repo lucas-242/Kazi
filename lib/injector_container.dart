488944dd5991447cd1991a4d68fbacfa55dd6b02
Lucas Guimarães <lucas.guimaraes@imusica.com.br>
2023-08-01 10:01:18 -0300

    refactor: Added new auth on injector container

--- lib/injector_container.dart
+++ lib/injector_container.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:kazi/app/data/local_storage/local_storage.dart';
+import 'package:kazi/app/services/api_service/api_service.dart';
+import 'package:kazi/app/services/api_service/http/http_api_service.dart';
+import 'package:kazi/app/services/auth_service/kazi_api/kazi_api_auth_service.dart';
 import 'package:kazi/app/services/services_service/services_service.dart';
 import 'package:kazi/app/services/time_service/local/local_time_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,7 +13,6 @@ import 'app/repositories/service_type_repository/service_type_repository.dart';
 import 'app/repositories/services_repository/firebase/firebase_services_repository.dart';
 import 'app/repositories/services_repository/services_repository.dart';
 import 'app/services/auth_service/auth_service.dart';
-import 'app/services/auth_service/firebase/firebase_auth_service.dart';
 import 'app/services/log_service/log_service.dart';
 import 'app/services/services_service/local/local_services_service.dart';
 import 'app/services/time_service/time_service.dart';
@@ -18,20 +20,47 @@ import 'app/services/time_service/time_service.dart';
 final serviceLocator = GetIt.instance;
 
 Future<void> initInjectorContainer() async {
+  _initExternal();
+  _initStorages();
+  _initServices();
+  _initRepositories();
+}
+
+Future<void> _initExternal() async {
   serviceLocator
       .registerSingleton<FirebaseFirestore>(FirebaseFirestore.instance);
+}
 
-  serviceLocator.registerSingleton<AuthService>(FirebaseAuthService());
-  serviceLocator.registerSingleton<TimeService>(LocalTimeService());
-  serviceLocator.registerSingleton<LogService>(LocalLogService());
-  serviceLocator.registerFactory<ServicesService>(
-    () => LocalServicesService(serviceLocator.get<TimeService>()),
-  );
-
+Future<void> _initStorages() async {
   serviceLocator.registerSingleton<LocalStorage>(
     SharedPreferencesLocalStorage(await SharedPreferences.getInstance()),
   );
+}
 
+Future<void> _initServices() async {
+  serviceLocator.registerSingleton<TimeService>(LocalTimeService());
+  serviceLocator.registerSingleton<LogService>(LocalLogService());
+
+  serviceLocator.registerSingleton<ApiService>(
+    HttpApiService(serviceLocator.get<LocalStorage>()),
+  );
+
+  serviceLocator.registerSingleton<AuthService>(
+    KaziApiAuthService(
+      apiService: serviceLocator.get<ApiService>(),
+      localStorage: serviceLocator.get<LocalStorage>(),
+      timeService: serviceLocator.get<TimeService>(),
+    ),
+  );
+
+  // serviceLocator.registerSingleton<AuthService>(FirebaseAuthService());
+
+  serviceLocator.registerFactory<ServicesService>(
+    () => LocalServicesService(serviceLocator.get<TimeService>()),
+  );
+}
+
+Future<void> _initRepositories() async {
   serviceLocator.registerFactory<ServicesRepository>(
     () => FirebaseServicesRepository(serviceLocator.get<FirebaseFirestore>()),
   );
