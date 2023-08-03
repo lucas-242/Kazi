bf0b6e1a61f079dfe79028dd8b4713e0b4c515b0
Lucas Guimarães <lucas.guimaraes@imusica.com.br>
2023-08-03 12:11:13 -0300

    refactor: Changed Kazi Api Service Type Repository name

--- lib/app/repositories/service_type_repository/kazi_api/kazi_api_service_type_repository.dart
+++ lib/app/repositories/service_type_repository/kazi_api/kazi_api_service_type_repository.dart
@@ -3,8 +3,8 @@ import 'package:kazi/app/repositories/service_type_repository/service_type_repos
 import 'package:kazi/app/services/api_service/api_service.dart';
 import 'package:kazi/app/shared/environment/environment.dart';
 
-final class KaziApiRepository implements ServiceTypeRepository {
-  KaziApiRepository(this._apiService);
+final class KaziApiServiceTypeRepository implements ServiceTypeRepository {
+  KaziApiServiceTypeRepository(this._apiService);
   final ApiService _apiService;
   final String url = '${Environment.instance.kaziApiUrl}servicetype';
 
