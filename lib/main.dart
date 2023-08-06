3d860ed027e0c44412920208ade610ab0bcbe9fb
Lucas Guimarães <guimaraeslucas242@gmail.com>
2023-08-06 17:27:49 -0300

    refactor: Removed Firebase from dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'app/app.dart';
-import 'app/services/log_service/log_service.dart';
 import 'app/core/constants/ad_keys.dart';
 import 'app/core/environment/environment.dart';
-import 'firebase_options.dart';
+import 'app/services/log_service/log_service.dart';
 import 'injector_container.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   MobileAds.instance.initialize();
   final configuration =
       RequestConfiguration(testDeviceIds: AdKeys.testDeviceIds);
