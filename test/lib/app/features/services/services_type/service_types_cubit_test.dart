dea84db27605574ae06703078fd6e97e35b6b78c
Lucas Guimarães <guimaraeslucas242@gmail.com>
2023-09-23 17:22:10 -0300

    fix: Fixed test imports

--- test/lib/app/features/services/services_type/service_types_cubit_test.dart
+++ test/lib/app/features/services/services_type/service_types_cubit_test.dart
@@ -6,13 +6,14 @@ import 'package:kazi/app/core/l10n/generated/l10n.dart';
 import 'package:kazi/app/core/utils/base_state.dart';
 import 'package:kazi/app/data/repositories/service_type_repository/service_type_repository.dart';
 import 'package:kazi/app/data/repositories/services_repository/services_repository.dart';
+import 'package:kazi/app/features/services/service_types/service_types.dart';
 import 'package:kazi/app/models/service_type.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 
-import '../../../../mocks/mocks.dart';
-import '../../../../utils/test_helper.dart';
-import '../services/service_form/service_form_cubit_test.mocks.dart';
+import '../../../../../mocks/mocks.dart';
+import '../../../../../utils/test_helper.dart';
+import '../service_form/service_form_cubit_test.mocks.dart';
 
 @GenerateMocks([ServiceTypeRepository, ServicesRepository, Auth])
 void main() {
