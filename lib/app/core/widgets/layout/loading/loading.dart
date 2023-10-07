6ec08aa15c828afffe63b712afb96cbf740d3d18
Lucas Guimarães <guimaraeslucas242@gmail.com>
2023-10-07 14:59:14 -0300

    fix: Fixed loading in the center

--- lib/app/core/widgets/layout/loading/loading.dart
+++ lib/app/core/widgets/layout/loading/loading.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:kazi/app/core/themes/themes.dart';
 
 class Loading extends StatelessWidget {
-  const Loading({super.key, this.color});
+  const Loading({super.key, this.color, this.height});
 
   final Color? color;
+  final double? height;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: context.height,
+      height: height,
       child: Center(
         child: CircularProgressIndicator(
           color: color,
