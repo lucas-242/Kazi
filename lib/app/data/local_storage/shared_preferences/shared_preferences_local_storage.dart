364e7bdebdf1390d1dbd00052b9963b23e69459c
Lucas Guimaraes <lucas.guimaraes@imusica.com.br>
2024-01-23 13:59:39 -0300

    fix: Fixed type_literal_in_constant_pattern

--- lib/app/data/local_storage/shared_preferences/shared_preferences_local_storage.dart
+++ lib/app/data/local_storage/shared_preferences/shared_preferences_local_storage.dart
@@ -11,13 +11,13 @@ class SharedPreferencesLocalStorage implements LocalStorage {
 
   @override
   Future<void> set<T>(String key, T value) {
-    switch (T) {
-      case String:
-        return _prefs.setString(key, value as String);
-      case bool:
-        return _prefs.setBool(key, value as bool);
-      case int:
-        return _prefs.setInt(key, value as int);
+    switch (value) {
+      case String _:
+        return _prefs.setString(key, value);
+      case bool _:
+        return _prefs.setBool(key, value);
+      case int _:
+        return _prefs.setInt(key, value);
       default:
         throw ClientError(
           'No support for the data type ${T.runtimeType}',
@@ -29,13 +29,13 @@ class SharedPreferencesLocalStorage implements LocalStorage {
   @override
   T? get<T>(String key) {
     switch (T) {
-      case String:
+      case const (String):
         return _prefs.getString(key) as T?;
-      case bool:
+      case const (bool):
         return _prefs.getBool(key) as T?;
-      case int:
+      case const (int):
         return _prefs.getInt(key) as T?;
-      case Object:
+      case const (Object):
         return _prefs.get(key) as T?;
       default:
         throw ClientError(
