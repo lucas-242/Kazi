7de279c25b5d0b15fd582a01f7db4103b876af61
Lucas Guimarães <guimaraeslucas242@gmail.com>
2024-12-24 12:00:00 -0300

    feat: Crashlytics added

--- lib/injector_container.dart
+++ lib/injector_container.dart
@@ -1,8 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:kazi/app/data/local_storage/local_storage.dart';
 import 'package:kazi/app/services/services_service/services_service.dart';
 import 'package:kazi/app/services/time_service/local/local_time_service.dart';
+import 'package:kazi/firebase_options.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'app/repositories/service_type_repository/firebase/firebase_service_type_repository.dart';
@@ -16,25 +21,54 @@ import 'app/services/time_service/time_service.dart';
 
 final serviceLocator = GetIt.instance;
 
-Future<void> initInjectorContainer() async {
-  serviceLocator
-      .registerSingleton<FirebaseFirestore>(FirebaseFirestore.instance);
+abstract class InjectorContainer {
+  static Future<void> init() async {
+    await _initGoogle();
+    await _initStorages();
+    _initServices();
+    _initRepositories();
+  }
 
-  serviceLocator.registerSingleton<AuthService>(FirebaseAuthService());
-  serviceLocator.registerSingleton<TimeService>(LocalTimeService());
-  serviceLocator.registerFactory<ServicesService>(
-    () => LocalServicesService(serviceLocator.get<TimeService>()),
-  );
+  static Future<void> _initGoogle() async {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
 
-  serviceLocator.registerSingleton<LocalStorage>(
-    SharedPreferencesStorage(await SharedPreferences.getInstance()),
-  );
+    FlutterError.onError = (errorDetails) {
+      FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+    };
 
-  serviceLocator.registerFactory<ServicesRepository>(
-    () => FirebaseServicesRepository(serviceLocator.get<FirebaseFirestore>()),
-  );
-  serviceLocator.registerFactory<ServiceTypeRepository>(
-    () =>
-        FirebaseServiceTypeRepository(serviceLocator.get<FirebaseFirestore>()),
-  );
+    PlatformDispatcher.instance.onError = (error, stack) {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+      return true;
+    };
+
+    await MobileAds.instance.initialize();
+
+    serviceLocator
+        .registerSingleton<FirebaseFirestore>(FirebaseFirestore.instance);
+  }
+
+  static Future<void> _initStorages() async {
+    serviceLocator.registerSingleton<LocalStorage>(
+      SharedPreferencesStorage(await SharedPreferences.getInstance()),
+    );
+  }
+
+  static Future<void> _initServices() async {
+    serviceLocator.registerSingleton<AuthService>(FirebaseAuthService());
+    serviceLocator.registerSingleton<TimeService>(LocalTimeService());
+    serviceLocator.registerFactory<ServicesService>(
+      () => LocalServicesService(serviceLocator.get<TimeService>()),
+    );
+  }
+
+  static void _initRepositories() {
+    serviceLocator.registerFactory<ServicesRepository>(
+      () => FirebaseServicesRepository(serviceLocator.get<FirebaseFirestore>()),
+    );
+    serviceLocator.registerFactory<ServiceTypeRepository>(
+      () => FirebaseServiceTypeRepository(
+          serviceLocator.get<FirebaseFirestore>()),
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:kazi/app/shared/utils/log_utils.dart';
 
 import 'app/app.dart';
 import 'app/shared/environment/environment.dart';
-import 'firebase_options.dart';
 import 'injector_container.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  MobileAds.instance.initialize();
-  await initInjectorContainer();
+  await InjectorContainer.init();
+
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp,
   ]);
+
   Log.flow('Environment: ${Environment.environmentValue}');
+
   runApp(const App());
 }
